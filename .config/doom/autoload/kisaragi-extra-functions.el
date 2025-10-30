d00ff5adcaa38eccc78e6d36bc4d423a1369d132
Kisaragi Hiu <mail@kisaragi-hiu.com>
2025-10-30 23:53:27 +0900

    basic lr moving commands

--- .config/doom/autoload/kisaragi-extra-functions.el
+++ .config/doom/autoload/kisaragi-extra-functions.el
@@ -1,10 +1,13 @@
-;; 2025-10-30T23:47:44+0900
+(defun k/wip-read-timestamp-right ()
+  "."
+  (interactive)
+  (re-search-forward "$\\|\\([:T+-]\\)" nil t))
 
 (defun k/wip-read-timestamp-left ()
   "."
   (interactive)
   (forward-char -1)
-  (re-search-backward "^\\|\\([:T-]\\)" nil t)
+  (re-search-backward "^\\|\\([:T+-]\\)" nil t)
   (when (match-string 1)
     (forward-char 1)))
 
@@ -15,4 +18,5 @@ Work in progress. The goal is to be like JS\\='s inquirer-date-prompt."
                         (format-time-string "%FT%T%z")
                         (let ((map (make-sparse-keymap)))
                           (define-key map (kbd "<left>") #'k/wip-read-timestamp-left)
-                          (define-key map (kbd "<right>") #'k/wip-read-timestamp-right))))
+                          (define-key map (kbd "<right>") #'k/wip-read-timestamp-right)
+                          map)))
