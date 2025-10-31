eadda1ca4b3e10a3abae758111f7da75631319e8
Kisaragi Hiu <mail@kisaragi-hiu.com>
2025-10-31 09:04:42 +0900

    package header etc.

--- read-timestamp.el
+++ read-timestamp.el
@@ -1,3 +1,35 @@
+;;; read-timestamp.el --- Read a tweakable timestamp -*- lexical-binding: t -*-
+
+;; Author: Kisaragi Hiu
+;; Maintainer: Kisaragi Hiu
+;; Version: 0.0.1
+;; Package-Requires: ((emacs "27.1"))
+;; Homepage: https://github.com/kisaragi-hiu/read-timestamp
+;; Keywords: extensions
+
+
+;; This file is not part of GNU Emacs
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+;;; Commentary:
+
+;; commentary
+
+;;; Code:
+
 ;; TODO: this needs to be unit tested
 (defun k/wip-read-timestamp--thing-at-point ()
   "."
@@ -124,3 +156,7 @@ Work in progress. The goal is to be like JS\\='s inquirer-date-prompt."
                           (define-key map (kbd "<up>") #'k/wip-read-timestamp-increment)
                           (define-key map (kbd "<down>") #'k/wip-read-timestamp-decrement)
                           map)))
+
+(provide 'read-timestamp)
+
+;;; read-timestamp.el ends here
